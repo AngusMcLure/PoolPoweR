f7a77654d35c783f4772c6dc09a88834da5fcd7f
fredjaya <fredjaya1@gmail.com>
2023-11-07 16:07:11 +1100

    Add unit tests for fi_pool_cluster. Includes errors with integrals and likelihood sums

--- tests/testthat/test-fisher_information.R
+++ tests/testthat/test-fisher_information.R
@@ -28,3 +28,93 @@ test_that("fi_pool() works with expected input ranges", {
     specificity = 1
   )))
 })
+
+test_that("fi_pool_cluster() outputs a 2x2 matrix for input vectors of length 2", {
+  expect_true(all.equal(
+    fi_pool_cluster(
+      s = c(10, 20),
+      N = c(10, 20),
+      prevalence = 0.9,
+      sensitivity = 1,
+      specificity = 1,
+      correlation = 0.5,
+      form = "beta",
+      real.scale = TRUE
+    ),
+    matrix(c(5.431112, -1.928655, -1.928655, 1.123625), nrow = 2),
+    tolerance = 1e-6
+  ))
+  expect_true(all.equal(
+    fi_pool_cluster(
+      s = c(5, 10),
+      N = c(10, 15),
+      prevalence = 0.9,
+      sensitivity = 1,
+      specificity = 1,
+      correlation = 0.5,
+      form = "beta",
+      real.scale = TRUE
+    ),
+    matrix(c(9.028451, -1.718530, -1.718530, 1.130158), nrow = 2),
+    tolerance = 1e-6
+  ))
+  expect_true(all.equal(
+    fi_pool_cluster(
+      s = c(5, 10),
+      N = c(10, 15),
+      prevalence = 0.7,
+      sensitivity = 0.95,
+      specificity = 0.8,
+      correlation = 0.9,
+      form = "cloglognorm",
+      real.scale = FALSE
+    ),
+    matrix(c(4.692347, -2.270677, -2.270677, 8.494053), nrow = 2),
+    tolerance = 1e-6
+  ))
+})
+
+test_that("fi_pool_cluster() fails when integral is divergent", {
+  expect_error(
+    fi_pool_cluster(
+      s = c(10, 20),
+      N = c(10, 100),
+      prevalence = 0.9,
+      sensitivity = 1,
+      specificity = 1,
+      correlation = 0.75,
+      form = "beta",
+      real.scale = FALSE
+    ),
+    "the integral is probably divergent"
+  )
+  expect_error(
+    fi_pool_cluster(
+      s = c(10, 20),
+      N = c(10, 100),
+      prevalence = 0.9,
+      sensitivity = 1,
+      specificity = 1,
+      correlation = 0.5,
+      form = "beta",
+      real.scale = FALSE
+    ),
+    "the integral is probably divergent"
+  )
+})
+
+test_that("fi_pool_cluster() fails when likelihoods or derivatives do not add up", {
+  expect_error(
+    fi_pool_cluster(
+      s = c(5, 10),
+      N = c(10, 50),
+      prevalence = 0.5,
+      sensitivity = 1,
+      specificity = 1,
+      correlation = 0.1,
+      form = "beta",
+      real.scale = FALSE
+    ),
+    "Error in integration of likelihoods. Likelihoods do not add to 1 or derivatives of likelihood with respect to parameters do not sum to 0"
+  )
+})
