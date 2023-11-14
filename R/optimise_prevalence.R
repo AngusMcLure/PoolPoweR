1f5c3ded29389ccd04437e44d540e629c18adec3
fredjaya <fredjaya1@gmail.com>
2023-11-14 16:26:08 +1100

    Style optimise_prevalence.R

--- R/optimise_prevalence.R
+++ R/optimise_prevalence.R
@@ -17,31 +17,31 @@ design_effect <- function(pool_size,
     ))[1, 1]
 }
 
-#optimising the pool size for estimating prevalence
+# optimising the pool size for estimating prevalence
 
-cost_fi <- function(s,prevalence,sensitivity,specificity, cost_unit, cost_pool){
-  (sum(cost_unit * s) + cost_pool)/fi_pool(s,prevalence,sensitivity,specificity)
+cost_fi <- function(s, prevalence, sensitivity, specificity, cost_unit, cost_pool) {
+  (sum(cost_unit * s) + cost_pool) / fi_pool(s, prevalence, sensitivity, specificity)
 }
 
-cost_fi_cluster <- function(s,N,prevalence,correlation,sensitivity,specificity, cost_unit, cost_pool,cost_cluster,form = 'beta'){
-  fi <- fi_pool_cluster(s,N,prevalence,correlation,sensitivity,specificity, form = form)
-  cost <- sum(cost_unit * s * N) + sum(cost_pool * N) +cost_cluster
-  #print(c(cost = cost, information = fi))
-  cost * solve(fi)[1,1]
+cost_fi_cluster <- function(s, N, prevalence, correlation, sensitivity, specificity, cost_unit, cost_pool, cost_cluster, form = "beta") {
+  fi <- fi_pool_cluster(s, N, prevalence, correlation, sensitivity, specificity, form = form)
+  cost <- sum(cost_unit * s * N) + sum(cost_pool * N) + cost_cluster
+  # print(c(cost = cost, information = fi))
+  cost * solve(fi)[1, 1]
 }
 
 
 
 optimise_s_prevalence <- function(prevalence, cost_unit, cost_pool,
-                                 cost_cluster = NA, correlation = NA,
-                                  N = 1, form = 'beta',
-                                  sensitivity = 1,specificity = 1,
-                                  max.s = 50, interval = 0){
-  if(form == 'discrete'){
+                                  cost_cluster = NA, correlation = NA,
+                                  N = 1, form = "beta",
+                                  sensitivity = 1, specificity = 1,
+                                  max.s = 50, interval = 0) {
+  if (form == "discrete") {
     stop('When form = "discrete" the cost of unit information function with respect to s often has mulitple minima and therefore the discrete distribution is not currently supported for optimisation')
   }
-  invalid.cost <- FALSE #trigger for when costs are infinite to ensure that there's no cost output in these cases
-  #print(c(theta = prevalence, sens = sensitivity, spec = specificity, unit = cost_unit, test = cost_pool, location =cost_cluster , rho = correlation, N = N, form = form, max.s = max.s))
+  invalid.cost <- FALSE # trigger for when costs are infinite to ensure that there's no cost output in these cases
+  # print(c(theta = prevalence, sens = sensitivity, spec = specificity, unit = cost_unit, test = cost_pool, location =cost_cluster , rho = correlation, N = N, form = form, max.s = max.s))
   theta <- prevalence
   ## The case cost_pool == Inf (or equivalently cost_unit = 0) is helpful because
   ## the s in this case (s_opt) is the largest you would ever want
@@ -50,7 +50,7 @@ optimise_s_prevalence <- function(prevalence, cost_unit, cost_pool,
   ## vectors. In practice this upper limit is usually really high (especially if
   ## prevalence is low and/or specificity is not perfect), so you can assume in
   ## most designs that you want to test all vectors
-  if(cost_pool == Inf){
+  if (cost_pool == Inf) {
     cost_unit <- 0
     cost_pool <- 1
     invalid.cost <- TRUE
@@ -63,114 +63,126 @@ optimise_s_prevalence <- function(prevalence, cost_unit, cost_pool,
   ## them. This gives you the lower bound for the pool size, i.e. even if you
   ## have budget to test them all individually you're actually better off
   ## testing them in pools of this size
-  if(cost_unit == Inf){
+  if (cost_unit == Inf) {
     cost_pool <- 0
     cost_unit <- 1
     invalid.cost <- TRUE
-
   }
-  if(is.na(correlation)){
-    cost <- function(x){
-      ufc <- cost_fi(x,theta,sensitivity,specificity, cost_unit, cost_pool)
-      ufc}
-  }else{
-    cost <- function(x){
-      ufc <- cost_fi_cluster(s = x, N = N, prevalence = theta,
-                                    correlation = correlation,
-                                    sensitivity = sensitivity,
-                                    specificity = specificity,
-                                    cost_unit = cost_unit, cost_pool = cost_pool,
-                                   cost_cluster =cost_cluster, form = form)
-      ufc}
+  if (is.na(correlation)) {
+    cost <- function(x) {
+      ufc <- cost_fi(x, theta, sensitivity, specificity, cost_unit, cost_pool)
+      ufc
+    }
+  } else {
+    cost <- function(x) {
+      ufc <- cost_fi_cluster(
+        s = x, N = N, prevalence = theta,
+        correlation = correlation,
+        sensitivity = sensitivity,
+        specificity = specificity,
+        cost_unit = cost_unit, cost_pool = cost_pool,
+        cost_cluster = cost_cluster, form = form
+      )
+      ufc
+    }
   }
 
-  opt <- optimise(cost,c(1,max.s))
+  opt <- optimise(cost, c(1, max.s))
   cost_opt_ceiling <- cost(ceiling(opt$minimum))
-  cost_opt_floor   <- cost(floor(opt$minimum))
+  cost_opt_floor <- cost(floor(opt$minimum))
 
-  if(cost_opt_ceiling < cost_opt_floor){
+  if (cost_opt_ceiling < cost_opt_floor) {
     s <- ceiling(opt$minimum)
     opt_cost <- cost_opt_ceiling
-  }else{
+  } else {
     s <- floor(opt$minimum)
     opt_cost <- cost_opt_floor
   }
 
-  if(s == max.s) warning('Maximum cost effectivness is achieved at or above the maximum size of pools allowed. Consider increasing max.s')
+  if (s == max.s) warning("Maximum cost effectivness is achieved at or above the maximum size of pools allowed. Consider increasing max.s")
 
-  if(interval == 0){
-    if(invalid.cost) opt_cost <- NA
+  if (interval == 0) {
+    if (invalid.cost) opt_cost <- NA
     return(list(s = s, cost = opt_cost, catch = s * N))
-  }else if(interval > 0){
+  } else if (interval > 0) {
     max_cost <- opt_cost * (1 + interval)
-    if(cost(1) < max_cost){
+    if (cost(1) < max_cost) {
       lower <- 1
-    } else{
-      lower <- ceiling(uniroot(function(x){cost(x) - max_cost},c(1,s))$root)
+    } else {
+      lower <- ceiling(uniroot(function(x) {
+        cost(x) - max_cost
+      }, c(1, s))$root)
     }
-    if(cost(max.s) < max_cost){
+    if (cost(max.s) < max_cost) {
       upper <- max.s
-      warning('A pool size greater than max.s may fall within the specified range of cost effectiveness. Consider increasing max.s')
-    }else{
-      upper <- floor(uniroot(function(x){cost(x) - max_cost},c(s,max.s))$root)
+      warning("A pool size greater than max.s may fall within the specified range of cost effectiveness. Consider increasing max.s")
+    } else {
+      upper <- floor(uniroot(function(x) {
+        cost(x) - max_cost
+      }, c(s, max.s))$root)
     }
-    if(invalid.cost){
+    if (invalid.cost) {
       opt_cost <- NA
-      cost_interval <- NA}
-    else{
-      cost_interval <- c(cost(lower),cost(upper))
+      cost_interval <- NA
+    } else {
+      cost_interval <- c(cost(lower), cost(upper))
     }
-    out <- list(s = s,
-                cost = opt_cost,
-                catch = s * N,
-                s_interval = c(lower, upper),
-                cost_interval = cost_interval,
-                catch_interval = N * c(lower, upper))
-  }else{
-    stop('interval must be between 0 and 1.')
+    out <- list(
+      s = s,
+      cost = opt_cost,
+      catch = s * N,
+      s_interval = c(lower, upper),
+      cost_interval = cost_interval,
+      catch_interval = N * c(lower, upper)
+    )
+  } else {
+    stop("interval must be between 0 and 1.")
   }
   out
 }
 
 optimise_sN_prevalence <- function(prevalence, cost_unit, cost_pool,
-                                  cost_cluster, correlation, form = 'beta',
+                                   cost_cluster, correlation, form = "beta",
                                    sensitivity = 1, specificity = 1,
-                                   max.s = 50, max.N = 20){
-  #print(c(theta = prevalence, sens = sensitivity, spec = specificity, unit = cost_unit, test = cost_pool, location =cost_cluster , rho = correlation, N = N, form = form, max.s = max.s))
+                                   max.s = 50, max.N = 20) {
+  # print(c(theta = prevalence, sens = sensitivity, spec = specificity, unit = cost_unit, test = cost_pool, location =cost_cluster , rho = correlation, N = N, form = form, max.s = max.s))
   theta <- prevalence
 
-  if(is.na(correlation) || correlation == 0){ #for simple random sampling there is no optimal N and for no correlation cluster survey the optimal approach is to infinite pools at one site (i.e. a simple random survey)
+  if (is.na(correlation) || correlation == 0) { # for simple random sampling there is no optimal N and for no correlation cluster survey the optimal approach is to infinite pools at one site (i.e. a simple random survey)
     opt <- optimise_s_prevalence(prevalence, cost_unit, cost_pool,
-                                  cost_cluster, correlation = NA,
-                                   N = 1, form = form,
-                                   sensitivity,specificity,
-                                   max.s = max.s)
+      cost_cluster,
+      correlation = NA,
+      N = 1, form = form,
+      sensitivity, specificity,
+      max.s = max.s
+    )
 
     opt$N <- ifelse(is.na(correlation), NA, Inf)
     opt$catch <- opt$N
-
-  }else{
+  } else {
     Nopt <- 1
     opt <- list(cost = Inf)
-    while(Nopt < max.N){
-      opt_new <- optimise_s_prevalence(prevalence,
-                                       cost_unit, cost_pool,cost_cluster,
-                                       correlation, Nopt+1, form,
-                                       sensitivity,specificity, max.s)
-      #print(opt_new)
-      if(opt_new$cost > opt$cost){
+    while (Nopt < max.N) {
+      opt_new <- optimise_s_prevalence(
+        prevalence,
+        cost_unit, cost_pool, cost_cluster,
+        correlation, Nopt + 1, form,
+        sensitivity, specificity, max.s
+      )
+      # print(opt_new)
+      if (opt_new$cost > opt$cost) {
         break
-      }else{
+      } else {
         Nopt <- Nopt + 1
         opt <- opt_new
       }
     }
     opt$N <- Nopt
-    if(opt$N == max.N){
-      warning('Maximum cost effectivness is achieved at or above the maximum number of pools allowed. Consider increasing max.N')
+    if (opt$N == max.N) {
+      warning("Maximum cost effectivness is achieved at or above the maximum number of pools allowed. Consider increasing max.N")
     }
-    if(opt$s == max.s){
-      warning('Maximum cost effectivness is achieved at or above the maximum size of pools allowed. Consider increasing max.s')
+    if (opt$s == max.s) {
+      warning("Maximum cost effectivness is achieved at or above the maximum size of pools allowed. Consider increasing max.s")
     }
   }
 
