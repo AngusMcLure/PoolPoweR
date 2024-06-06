224a87190fe2b8d258f3994f130b9ac81f9f8e18
AngusMcLure <angus.mclure@anu.edu.au>
2024-06-06 21:41:26 +1000

    consistent method documentation

--- R/pool_strat_methods.R
+++ R/pool_strat_methods.R
@@ -1,18 +1,39 @@
-#' Methods for pool_strat obejcts
+#' Constructor for pool_strat objects
 #'
-#' @param x An object of class `pool_strat` (typically produced by one of the
-#'   built-in functions for generating them such as `pool_max_size()` and
-#'   `pool_taget_number()`
-#' @param ... Ignored
+#' @param strat function that takes an integer number of units and returns the
+#'   number and size of pools to split the units across
+#' @param call call used to generate the pool_strat function. Used for format
+#'   and as.character method (pretty printing)
+#' @param description text description of pool strategy to be used in print
+#'   function
 #' @rdname pool_strat_methods
+
+pool_strat <- function(strat, call, description){
+  if(!inherits(strat, 'function')) {stop('strat must be a function')}
+  ps <- strat
+  attr(ps, 'call') <- call
+  attr(ps, 'description') <- description
+  class(ps) <- c('pool_strat', 'function')
+  return(ps)
+}
+
 #' @export
+format.pool_strat <- function(x,...){
+  return(format(attr(x,'call'),...))
+}
 
+#' @export
+as.character.pool_strat <- function(x,...){
+  return(format(attr(x,'call'),...))
+}
 
-
+#' @export
 print.pool_strat <- function(x,...){
   if(is.null(attr(x, 'description'))){
     print.function(x)
   }else{
-    print(paste0('A pooling strategy that ', attr(x, 'description')))
+    cat(paste0('A pooling strategy that ', attr(x, 'description')))
   }
 }
+
+
--- R/power_utils.R
+++ R/power_utils.R
@@ -1,14 +1,14 @@
-#' S3 Class and Methods for Power and Sample Size Calculation Outputs
+#' S3 class for power and sample size calculation outputs with pooled tests
 #'
-#' As many variables are shared across groups of functions (e.g. `power_pool` 
-#' and `sample_size_pool`), this class and print method ensures that outputs 
-#' are stored and displayed consistently. Especially important to reduce the
-#' business logic for the Shiny app.  
-#' 
+#' As many variables are shared across groups of functions (e.g. `power_pool`
+#' and `sample_size_pool`), this class and print method ensures that outputs are
+#' stored and displayed consistently, summarising calculations results and the
+#' inputs for context.
+#'
 #' @param ... Input and inferred variables from relevant functions.
 #'
 #' @return An object of class \code{power_size_results} containing selected
-#' input parameters and results.
+#'   input parameters and results.
 #' @export
 #'
 #' @examples
@@ -17,6 +17,7 @@
 #'                              prev_null = theta0, prev_alt = theta0, sig_level = sig_level,
 #'                              power = power, alternative = alternative, target = "power")
 #' print(result)
+
 power_size_results <- function(pool_size, pool_number, cluster_number,
                                prev_null, prev_alt, sig_level, power,
                                alternative, target) {
@@ -37,18 +38,9 @@ power_size_results <- function(pool_size, pool_number, cluster_number,
   return(results)
 }
 
-#' Print Method for power_size_results
-#'
-#' @param x An object of class \code{power_size_results}
-#' @export
-print <- function(x) {
-  UseMethod("print")
-}
-
-#' @rdname print.power_size_results
 #' @method print power_size_results
 #' @export
-print.power_size_results <- function(x) {
+print.power_size_results <- function(x,...) {
   # Remove 'target' from instance to decorate line of the
   # inferred variable(s)
   target_idx <- which(names(x) == "target")
@@ -62,4 +54,5 @@ print.power_size_results <- function(x) {
   # adapted from `stats::power.prop.test`
   cat(paste(format(names(x), width = 15L, justify = "right"),
             format(x), sep = " = "), sep = "\n")
+  invisible(x)
 }
