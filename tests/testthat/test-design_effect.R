e8608680e86ca07cef428b87591414262d2232e7
Fred Jaya <36436914+fredjaya@users.noreply.github.com>
2024-06-26 11:35:03 +1000

    DEV: Implement design_effect for fixed design and update tests

--- tests/testthat/test-design_effect.R
+++ tests/testthat/test-design_effect.R
@@ -1,52 +1,31 @@
-### design_effect() -----------------------------------------------------------
+# fixtures ----
+fd <- fixed_design(
+  pool_size = 5, pool_number = 10, sensitivity = 0.99, specificity = 0.95
+)
+
+# fixed_design ----
 test_that("design_effect() gives consistent output for basic tests", {
   # This one has reasonable inputs
   expect_equal(
-    design_effect(
-      pool_size = 5,
-      pool_number = 10,
-      prevalence = 0.01,
-      correlation = 0.05,
-      sensitivity = 0.99,
-      specificity = 0.95),
+    design_effect(fd, prevalence = 0.01, correlation = 0.05),
     0.7240988, tolerance = 1e-7
   )
   expect_equal(
-    design_effect(
-      pool_size = 10,
-      pool_number = 10,
-      prevalence = 0.9,
-      sensitivity = 1,
-      specificity = 1,
-      correlation = 0.9,
-      form = "beta"
-    ),
-    118.9243,
-    tolerance = 1e-4
+    design_effect(fixed_design(10, 10), prevalence = 0.9, correlation = 0.9),
+    118.9243, tolerance = 1e-4
   )
   expect_equal(
     design_effect(
-      pool_size = 10,
-      pool_number = 10,
-      prevalence = 0.2,
-      sensitivity = 0.9,
-      specificity = 0.8,
-      correlation = 0.2,
-      form = "beta"
-    ),
-    26.50055,
-    tolerance = 1e-5
+      fixed_design(10, 10, 0.9, 0.8), prevalence = 0.2, correlation = 0.2), 
+      26.50055, tolerance = 1e-5
   )
 })
 
 test_that("design_effect() fails for some very unusual parameters because integral in call to fi_pool_cluster() appears starts to have numeric issues", {
   expect_error(
     design_effect(
-      pool_size = 100,
-      pool_number = 10,
+      fixed_design(100, 10, 1, 0.7),
       prevalence = 0.8,
-      sensitivity = 1,
-      specificity = 0.7,
       correlation = 0.8,
       form = "cloglognorm"
     ),
@@ -54,13 +33,5 @@ test_that("design_effect() fails for some very unusual parameters because integr
   )
 })
 
-
-test_that("bad inputs caught in design_effect()", {
-  expect_error(design_effect(pool_size = TRUE), "TRUE is a logical")
-  expect_error(design_effect(pool_size = 5, pool_number = 10, prevalence = 10), "10 is > 1")
-  expect_error(design_effect(pool_size = 5, pool_number = 10, prevalence = 0.01, correlation = 0.1, sensitivity = 1, specificity = 1, form = "binomal"),
-               "form must be one of 'beta', 'logitnorm', 'cloglognorm', or 'discrete'.")
-})
-
-### design_effect_random() ----------------------------------------------------
+### design_effect_random() ----
 
