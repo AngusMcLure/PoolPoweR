b8edb792b6a8b8713381c6fc88f4a9254f1da95b
Fred Jaya <36436914+fredjaya@users.noreply.github.com>
2024-07-08 10:44:24 +1000

    TST: Allow check_input2 functions to take NA.
    e.g. for correlation and cost_cluster, instead of rewriting new
    functions for these only

--- R/check_input2.R
+++ R/check_input2.R
@@ -1,19 +1,40 @@
-check_geq2 <- function(val, min) {
+#' Check input helpers
+#'
+#' Helper functions to validate input parameters.
+#' 
+#' @param val numeric/NA Input parameter
+#' @param min numeric for check_geq2
+#' @param allow_na boolean whether NAs are acceptable inputs or not. i.e. for 
+#' clustering cases correlation/cost_cluster are NA
+check_geq2 <- function(val, min, allow_na = FALSE) {
   name <- deparse(substitute(val)) # get name of variable
-  if (!is.numeric(val)) {
-    stop(glue::glue("{name} must be numeric, not {class(val)}."))
+  if (allow_na) {
+  	if (!is.numeric(val) & !is.na(val)) {
+  	  stop(glue::glue("{name} must be numeric or NA, not {class(val)}."))
+  	}
+  } else { # allow_na = FALSE
+  	if (!is.numeric(val)) {
+  	  stop(glue::glue("{name} must be numeric, not {class(val)}."))
+  	}
   }
-  if (val < min) {
+  if (is.numeric(val) && val < min) {
     stop(glue::glue("{name} must be >= {min}."))
   }
 }
 
-check_in_range2 <- function(val) {
+#' @rdname check_geq2
+check_in_range2 <- function(val, allow_na = FALSE) {
   name <- deparse(substitute(val)) # get name of variable
-  if(!is.numeric(val)) {
-    stop(glue::glue("{name} must be numeric, not {class(val)}."))
+  if (allow_na) {
+  	if (!is.numeric(val) & !is.na(val)) {
+  	  stop(glue::glue("{name} must be numeric or NA, not {class(val)}."))
+    }
+  } else {
+  	if (!is.numeric(val)) {
+  	  stop(glue::glue("{name} must be numeric, not {class(val)}."))
+  	}
   }
-  if (val < 0 | val > 1) {
+  if (is.numeric(val) && (val < 0 || val > 1)) {
     message(glue::glue("{name} must be a numeric value between 0 and 1, inclusive."))
     stop(glue::glue("{name} = {val}"))
   }
--- tests/testthat/test-check_input2.R
+++ tests/testthat/test-check_input2.R
@@ -6,6 +6,11 @@ test_that("check_geq2", {
   expect_error(check_geq2(y, 1), "y must be numeric, not character.")
 })
 
+test_that("check_geq2 allow_na", {
+  expect_silent(check_geq2(NA, 0, allow_na = TRUE))
+  expect_error(check_geq2(NA, allow_na = FALSE))
+})
+
 test_that("check_in_range2", {
   x <- 0.01
   expect_silent(check_in_range2(x))
@@ -15,4 +20,9 @@ test_that("check_in_range2", {
   expect_error(check_in_range2(x), "x = 2")
   y <- "a"
   expect_error(check_in_range2(y), "y must be numeric, not character.")
+})
+
+test_that("check_in_range2 allow_na", {
+  expect_silent(check_in_range2(val = NA, allow_na = TRUE))
+  expect_error(check_in_range2(NA, allow_na = FALSE))
 })
